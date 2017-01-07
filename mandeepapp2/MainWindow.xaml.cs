b07d185faaf4e202ff7451b3c0664b68758274e2
Mandeep <Mandeep@DESKTOP-MANDEEP>
2017-01-06 20:28:02 -0600

    Add project files.

---
+++ mandeepapp2/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mandeepapp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void clickbutton(object sender, RoutedEventArgs e)
+        {
+            WebClient weather = new WebClient();
+            weather.BaseAddress = "http://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=555fa8f3d3204ad49120a43f2479e7d4";
+            string weatherData = weather.DownloadString(weather.BaseAddress);
+            jsonResponse weathDataObject = JsonConvert.DeserializeObject<jsonResponse>(weatherData);
+            textBox.Text = weathDataObject.name;
+        
+        }
+
+        public class Coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+
+        public class Weather
+        {
+            public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        public class Main
+        {
+            public double temp { get; set; }
+            public int pressure { get; set; }
+            public int humidity { get; set; }
+            public double temp_min { get; set; }
+            public double temp_max { get; set; }
+        }
+
+        public class Wind
+        {
+            public double speed { get; set; }
+            public int deg { get; set; }
+        }
+
+        public class Clouds
+        {
+            public int all { get; set; }
+        }
+
+        public class Sys
+        {
+            public int type { get; set; }
+            public int id { get; set; }
+            public double message { get; set; }
+            public string country { get; set; }
+            public int sunrise { get; set; }
+            public int sunset { get; set; }
+        }
+
+        public class jsonResponse
+        {
+            public Coord coord { get; set; }
+            public List<Weather> weather { get; set; }
+            public string @base { get; set; }
+            public Main main { get; set; }
+            public int visibility { get; set; }
+            public Wind wind { get; set; }
+            public Clouds clouds { get; set; }
+            public int dt { get; set; }
+            public Sys sys { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public int cod { get; set; }
+        }
+
+    }
+}
